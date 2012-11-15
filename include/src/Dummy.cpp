01b9aac3b302fd950d7ea4694703383b8e1c37cd
Martin Uhrin <martin.uhrin@gmail.com>
2012-11-15 22:45:17 +0000

    Tidied up pipelib

--- include/src/Dummy.cpp
+++
@@ -1,8 +0,0 @@
-/**
-/* This is an unused cpp file to ensure that CMake properly generates
-/* project files for VS, XCode etc as add_library will not work without
-/* a .cpp file present.
-/* Being a header library there is, of course, no need to build it, this
-/* is just supplied as a convenience.
-/*
-/**/
